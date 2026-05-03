7ad0b74a4bbdf0d5fb893b72bcb8a9525c31e95e
ISLOMBEK201009 <kompyuter701@gmail.com>
2026-05-03 12:31:27 +0500

    kino-bot
    1000$ dollorli

---
+++ 2 dars/main.js
@@ -0,0 +1,45 @@
+
+// dom ---html
+// elementlari  -- all the code
+
+// let 
+// document --- body -  html
+// query -- kalit
+//  selector--- tanlovchi
+
+//  - 
+// camel/case
+// snack/case 
+//  let h = document.querySelector("h1")
+//  let some = document.querySelector(".menu")
+//  function app() {
+//  h.style.color = "red";
+//  h.style.backgroundColor ="green";
+// some.style.width = "100%";
+// some.style.backgroundColor = "red";
+//  }
+// let div = document.querySelector(".box")
+
+// function ochil() {
+//     div.style.cssText = "transform: translateX(0px); transition: 1s;"
+
+// }
+// function yopil() {
+//     div.style.cssText = "transform: translateX(-310px); transition: 1s;"
+// }
+let div = document.querySelector(".rabb")
+
+function ong() {
+    div.style.cssText = "transform: translateX(-100px); transition: 1s;"
+
+}
+function chap() {
+    div.style.cssText = "transform: translateX(100px); transition: 1s;"
+}
+function yuqori() {
+    div.style.cssText = "transform: translateY(-100px); transition: 1s;"
+
+}
+function past() {
+    div.style.cssText = "transform: translateY(100px); transition: 1s;"
+}
